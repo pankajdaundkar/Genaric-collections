b11cbf27a07270c598731c61314a1116110b9956
pankajdaundkar <phd27993@gmail.com>
2023-07-24 13:45:30 +0530

    Add project files.

---
+++ Genaric collections/Program.cs
@@ -0,0 +1,97 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genaric_collections
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("______list(product)_________");
+
+         List < Product> p1 = new List<Product>();
+            p1.Add(new Product { id = 1, name = "mobile", price = 10000 });
+            p1.Add(new Product { id = 2, name = "laptop", price = 15000 });
+            p1.Add(new Product { id = 3, name = "tab", price = 20000 });
+
+           // p1.AddRange(p1);
+
+            p1.Remove(new Product { id = 2, name = "laptop", price = 15000 }); 
+
+            p1.Insert(0, new Product { id = 4, name = "pankaj", price = 15000 });
+
+            foreach (Product p in p1)
+            {
+                Console.WriteLine(p.PrintDetails());
+            }
+            Console.WriteLine("________list(int)_______________");
+
+            List<int> p2 = new List<int>();
+            p2.Add(5);
+            p2.Add(6);
+            foreach (int p in p2)  
+            {
+                Console.WriteLine(p);
+            }
+            Console.WriteLine("________list(string)__________________");
+
+            List<string> list = new List<string>();
+            list.Add("pankaj");
+            list.Add("sagar");
+            list.Add("pratik");
+
+            list.Insert(2, "laptop");
+
+            list.Remove("sagar");
+
+            list.AddRange(list);
+
+            foreach (string s in list)
+            {
+                Console.WriteLine(s);
+
+            }
+            Console.WriteLine("___________stack_________________________");
+
+            Stack<int> stack = new Stack<int>();
+            stack.Push(10);
+            stack.Push(20);
+            stack.Push(30);
+
+            //stack.Pop();
+            stack.Peek();
+            stack.Contains(12);
+
+            foreach (int p in stack)
+            {
+                Console.WriteLine(p);
+            }
+            Console.WriteLine("____________queue____________________________");
+
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(10);
+            queue.Enqueue(50);
+            queue.Enqueue(34);
+
+            queue.Dequeue();
+
+            queue.Peek();
+
+            //queue.Clear();
+
+            foreach (int r in queue)
+            {
+                Console.WriteLine(r);
+            }
+
+
+            
+           
+        }
+    }
+}
---
+++ Genaric collections/product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genaric_collections
+{
+    public class Product
+    {
+        public int id { set; get; }
+        public string name { set; get; }
+        public int price { set; get; }
+
+
+        public string PrintDetails()
+        {
+            return($"product id {id}, name={name}, price={price}");
+        }
+
+    }
+}
